a00dedd37780e6da8c8aa17584feb8845c7f3f83
Hareesh S <hareesh.s.teja@gmail.com>
2022-11-01 22:43:11 +0530

    Init Repo

---
+++ DashboardApp/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import HomeScreen from './src/Screens/HomeScreen';
+
+export default function App() {
+  return(
+    <>
+    <HomeScreen />
+    </>
+  )
+}
